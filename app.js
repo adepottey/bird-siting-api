e456c67721fc3215c5317f3cab54934986e0ffcc
Ashley DePottey <adepottey@esri.com>
2022-07-10 16:01:47 -0700

    Add express app with /sitings route

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const sitings = require("./sitings.json");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome my bird sitings API!");
+});
+
+app.get("/sitings", (req, res) => {
+  res.status(200).json(sitings);
+});
+
+app.listen(port, () => {
+  console.log(`Bird siting API listening on port ${port}`);
+});
