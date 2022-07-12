c748a51a396ebeb843e1e6a3504f5916f8783d80
Ashley DePottey <adepottey@esri.com>
2022-07-11 18:24:07 -0700

    Cleanup, add more sightings

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 var cors = require("cors");
-const sitings = require("./sitings.json");
+const sightings = require("./sightings.json");
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome my bird sitings API!");
+  res.send("Welcome my bird sightings API!");
 });
 
-app.get("/sitings", (req, res) => {
-  res.status(200).json(sitings);
+app.get("/sightings", (req, res) => {
+  res.status(200).json(sightings);
 });
 
 app.listen(port, () => {
-  console.log(`Bird siting API listening on port ${port}`);
+  console.log(`Bird sighting API listening on port ${port}`);
 });
